1e167198a7e0fcc3307c1f6db6f080452a64f10e
Hmmmza78 <86945130+Hmmmza78@users.noreply.github.com>
2021-11-01 17:18:17 +0500

    pp

---
+++ magic.js
@@ -0,0 +1,14 @@
+// console.log("Hello dev");
+// const ulNav = document.querySelectorAll(".nav-item");
+// console.log(ulNav);
+// ulNav.forEach(function (params) {
+//     console.log(params);
+//     params.classList.add("mx-2")
+// })
+// const a = document.querySelectorAll(".links")
+// console.log(a);
+// a.forEach(function (params) {
+//     // console.log(params);
+//     params.classList.add("text-primary")
+
+// })
